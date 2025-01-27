b1e3fa47739fa7f439bd5ea692c7c69331573513
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-27 22:54:10 +0200

    Added the actual apiKey and apiUrl

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class DeepSeekConstants {
-  static const String apiKey = '';
-  static const String apiUrl = '';
+  static const String apiKey = 'sk-ac68cbe7fc2c407bb80da6ef3666cea8';
+  static const String apiUrl = 'https://api.deepseek.com/v1/generate';
 }
