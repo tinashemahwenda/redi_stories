029d4bea0933e69e48d940ecc1c148566d70d7b1
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-27 23:13:58 +0200

    Created a story page and a function to load story

---
+++ lib/story_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:redi_stories/services/api_services.dart';
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  String story = '';
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> loadStory() async {
+    try {
+      final generateStory = await ApiServices.generateStory();
+      setState(() {
+        story = generateStory;
+        print(story);
+      });
+    } catch (e) {
+      story = 'Failed to load story';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
